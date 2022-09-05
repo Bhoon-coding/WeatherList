83248b268f04eba735414f2851e76879973052e2
bhoon-coding <bhooncoding@gmail.com>
2022-09-05 17:55:16 +0900

    [#1] feat: 초기세팅 (코드 베이스 UI 세팅)

---
+++ WeatherList/Application/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  WeatherList
+//
+//  Created by BH on 2022/09/05.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
