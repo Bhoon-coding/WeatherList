5ec1c23f0a05f4561acc33e8c87ff31219dbea14
bhoon-coding <bhooncoding@gmail.com>
2022-09-06 16:41:04 +0900

    [#4] Feat: Weather Model 생성

---
+++ WeatherList/Models/WeatherResponse.swift
@@ -0,0 +1,51 @@
+//
+//  WeatherResponse.swift
+//  WeatherList
+//
+//  Created by BH on 2022/09/06.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    
+    let list: [WeatherInfo]
+    
+}
+
+struct WeatherInfo: Decodable {
+    
+    let temp: Temp
+    let weather: [Weather]
+    let date: String
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case temp = "main"
+        case weather
+        case date = "dt_txt"
+        
+    }
+    
+}
+
+struct Temp: Decodable {
+    
+    let tempMin: Double
+    let tempMax: Double
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+    
+}
+
+struct Weather: Decodable {
+    
+    let id: Int
+    let main: String
+    let icon: String
+    
+}
